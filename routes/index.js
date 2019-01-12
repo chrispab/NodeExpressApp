a5a60a56c800cd851bcf68dba90a1c89908e08ac
chrispab <cbattisson@gmail.com>
2019-01-12 19:01:14 +0000

    update

--- routes/index.js
+++ routes/index.js
@@ -40,8 +40,8 @@ function hasDuplicates(array) {
 }
 
 // 
-r = 1.6180339887;
-num1 = r - 1;
+seed = 1.6180339887;
+num1 = seed - 1;
 
 
 function getNumbers(drawType) {
@@ -79,40 +79,51 @@ function getNumbers(drawType) {
         mainBalls = [numMainBalls];
     }
 
-    do {
-        remover = 34;
-        remover2 = 16;
-        //res.write('<br>');
-        for (i = 0; i < numMainBalls; i++) {
-            //for (i=0; )
-            num = Math.round(Math.random() * (mainBallsRange - 1) + 1);
-            //num = Math.round(Math.random() * r * (mainBallsRange - 1) + 1) - remover;
-            //Math.random() * (max - min) + min;
 
-            mainBalls[i] = num;
-            console.log(num);
-            //res.write(num.toString() + '<br>'); //write a response to the client
-        }
-        if (hasDuplicates(mainBalls)) {
-            console.log("Dupe(s) found in main balls - redraw");
-        }
-    } while (hasDuplicates(mainBalls));
+    //loop for random num of times bet 1 and 100
+    loops1 = Math.round(Math.random() * (seed * 1000 - 1) + 1);
+    loops2 = Math.round(Math.random() * (seed * 1000 - 1) + 1);
 
-    do {
-        //res.write('<br>') //write a response to the client
-        console.log("");
-        for (i = 0; i < numExtraBalls; i++) {
-            num = Math.round(Math.random() * (extraBallsRange - 1) + 1);
+    for (let index = 0; index < loops1; index++) {
+        do {
+            remover = 34;
+            remover2 = 16;
+            //res.write('<br>');
+            for (i = 0; i < numMainBalls; i++) {
+                //for (i=0; )
+                num = Math.round(Math.random() * (mainBallsRange - 1) + 1);
+                //num = Math.round(Math.random() * r * (mainBallsRange - 1) + 1) - remover;
+                //Math.random() * (max - min) + min;
 
-            //num = Math.round(Math.random() * r * (extraBallsRange - 1) + 1) - remover2;
-            extraBalls[i] = num;
-            console.log(num);
-            //res.write(num.toString() + '<br>'); //write a response to the client
-        }
-        if (hasDuplicates(extraBalls)) {
-            console.log("Dupe(s) found in extra balls - redraw");
-        }
-    } while (hasDuplicates(extraBalls));
+                mainBalls[i] = num;
+                console.log(num);
+                //res.write(num.toString() + '<br>'); //write a response to the client
+            }
+            if (hasDuplicates(mainBalls)) {
+                console.log("Dupe(s) found in main balls - redraw");
+            }
+        } while (hasDuplicates(mainBalls));
+    }
+
+    for (let index = 0; index < loops2; index++) {
+
+        do {
+            //res.write('<br>') //write a response to the client
+            console.log("");
+            for (i = 0; i < numExtraBalls; i++) {
+                num = Math.round(Math.random() * (extraBallsRange - 1) + 1);
+
+                //num = Math.round(Math.random() * r * (extraBallsRange - 1) + 1) - remover2;
+                extraBalls[i] = num;
+                console.log(num);
+                //res.write(num.toString() + '<br>'); //write a response to the client
+            }
+            if (hasDuplicates(extraBalls)) {
+                console.log("Dupe(s) found in extra balls - redraw");
+            }
+        } while (hasDuplicates(extraBalls));
+    }
+
     mainBalls.sort(function(a, b) {
         return a - b
     });
